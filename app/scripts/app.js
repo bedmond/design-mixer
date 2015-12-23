366c8eb66830c62e1fcfd5b0f5cd5d1b1f0eab98
BrianEdmond <brianwedmond@gmail.com>
2015-12-23 13:14:58 -0500

    Added Register, Login, and Upload Images pages.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,23 @@
-//Need to show different images. How?
+//Placeholder images for styling and testing
 var thumb = {
   src: "https://farm5.staticflickr.com/4074/4915795521_40b2faf863_b.jpg"
 };
 
 var designMixer = angular.module('DesignMixer', ['ui.router', 'firebase']);
+
+designMixer.run(['$rootScope', '$state', function($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('login');
+    }
+  });
+}]);
+
+designMixer.factory('Auth', ['$firebaseAuth', 'FIREBASE_URL', function ($firebaseAuth, FIREBASE_URL) {
+  
+  var ref = new Firebase(FIREBASE_URL);
+  return $firebaseAuth(ref);
+}]);
 
 designMixer.config(['$stateProvider', '$locationProvider', function ($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
@@ -20,49 +34,109 @@ designMixer.config(['$stateProvider', '$locationProvider', function ($stateProvi
     templateUrl: '/templates/detail.html'
   });
 
+  $stateProvider.state('register', {
+    url: '/register',
+    controller: 'Register.controller',
+    templateUrl: '/templates/register.html',
+  });
+
+  $stateProvider.state('login', {
+    url: '/login',
+    controller: 'Login.controller',
+    templateUrl: '/templates/login.html',
+    resolve: {
+      "currentAuth": ['Auth', function (Auth) {
+        return Auth.$waitForAuth();
+      }]
+    }
+  });
+
   $stateProvider.state('upload', {
     url: '/upload',
     controller: 'Upload.controller',
-    templateURL: '/templates/upload.html'
+    templateUrl: '/templates/upload.html',
+    resolve: {
+      "currentAuth": ['Auth', function (Auth) {
+        return Auth.$requireAuth();
+      }]
+    }
   });
 
 
   }]);
+
 designMixer.constant('FIREBASE_URL', 'https://designmixer.firebaseio.com');
 
-
+//Search display page. Displays images that have been uploaded.
 designMixer.controller('Thumbs.controller', ['$scope', 'FIREBASE_URL', '$firebaseArray', function ($scope, FIREBASE_URL, $firebaseArray) {
 
-  //Grab image url from storage to display after upload tool adds an image.
-  //Replaces JS at the top of the page.
-  //var ref = new Firebase(FIREBASE_URL);
-  //var id = ~~(Math.random() * 10000);
-
-  //$scope.thumbs = $firebaseArray(ref)
-
-  //Build the firebaseArray.
-  //$scope.thumbs = function() {
-    //var name = $scope.thumb;
-    //$scope.thumbs.$add({
-      //name: $scope.thumb,
-      //src:  "/assets/images/*.{png,jpg,jpeg,tiff} + id"
-      //created_at: Firebase.ServerValue.TIMESTAMP
-  //});
-
+  //replicates sample image for styling and testing
   $scope.thumbs = [];
     for (var i = 0; i < 36; i++) {
       $scope.thumbs.push(angular.copy(thumb));
     }
+
 }]);
 
-
-
+//Detail page displayed when thumb is clicked on.
 designMixer.controller('Detail.controller', ['$scope', 'FIREBASE_URL', '$firebaseArray', function ($scope, FIREBASE_URL, $firebaseArray) {
+  
   $scope.detail = angular.copy(thumb)
 }]);
 
+//Register page, required for login.
+designMixer.controller('Register.controller', ['$scope', '$firebaseArray', 'Auth', function ($scope, $firebaseArray, Auth) {
 
+  $scope.createUser = function() {
+    $scope.message = null;
+    $scope.error = null;
 
+    Auth.$createUser({
+      email: $scope.email,
+      password: $scope.password,
+    }).then(function(userData) {
+      console.log("User created with uid: " + userData.uid);
+    }).catch(function(error) {
+      $scope.error = error;
+      console.log("Error: ", error);
+    });
+  }
+
+}]);
+
+//Login page, required for upload of images.
+designMixer.controller('Login.controller', ['$scope', 'FIREBASE_URL', '$firebaseAuth', 'currentAuth', 'Auth', '$firebaseArray', function ($scope, FIREBASE_URL, $firebaseAuth, currentAuth, Auth, $firebaseArray) {
+
+  $scope.auth = Auth;
+
+  $scope.auth.$onAuth(function(authData) {
+    $scope.authData = authData;
+  });
+
+  var ref = new Firebase(FIREBASE_URL);
+  $scope.authObj = $firebaseAuth(ref);
+
+  $scope.login = function() {
+    $scope.message = null;
+    $scope.error = null;
+
+    $scope.authObj.$authWithPassword({
+      email: $scope.email,
+      password: $scope.password
+    }).then(function(authData) {
+      console.log("Logged in as: ", authData.uid);
+    }).catch(function(error) {
+      console.error("Authentication failed: ", error);
+    });
+  }
+
+  $scope.logout = function() {
+    ref.unauth();
+  };
+
+}]);
+
+//Upload page, must be registered and logged in.
 designMixer.controller('Upload.controller', ['$scope', 'FIREBASE_URL', '$firebaseArray', function ($scope, $FIREBASE_URL, $firebaseArray) {
 
   $scope.uploadImage = function() {
